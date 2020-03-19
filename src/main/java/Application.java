234e623c466ee437eaca7bb5c1315a512bff9cc9
prokhorovartem <art.98.pro@gmail.com>
2020-03-19 23:26:42 +0300

    removed property file and init Bot

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,21 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try {
+            ApiContextInitializer.init();
+
+            TelegramBotsApi botsApi = new TelegramBotsApi();
+
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+
+            botsApi.registerBot(new Bot(botOptions));
+
+        } catch (TelegramApiRequestException ignored) {
+        }
     }
 }
---
+++ src/main/java/Bot.java
@@ -0,0 +1,38 @@
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public final class Bot extends TelegramLongPollingCommandBot {
+
+    private static final String BOT_NAME = "Da Ni Ar";
+    private static final String BOT_TOKEN = "961694457:AAHrgGPDkBsvWzQ-mNWQr9smAzzx9j8XKaI";
+
+    public Bot(DefaultBotOptions botOptions) {
+        super(botOptions, BOT_NAME);
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        Message message = update.getMessage();
+        SendMessage answer = new SendMessage();
+        answer.setText("Привет");
+        answer.setChatId(message.getChatId());
+        replyToUser(answer);
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    private void replyToUser(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException ignored) {
+        }
+    }
+}
